a92cb51878f5e4561dbb88d5e7ea8e3f71d140f6
shinn lancelot <945226793@qq.com>
2018-09-18 17:25:57 +0800

    1. 添加example及相应文件 2. 插件基本结构架构完成

---
+++ example/easyuploader.js
@@ -0,0 +1,43 @@
+(function() {
+    'use strict';
+
+    var easyUploader = function(options) {
+        if (!(this instanceof easyUploader)) {
+            return new easyUploader(options);
+        }
+
+        // default options object
+        var defaultOptions = {
+            "el": "#file",
+        }
+
+        // extend options
+        this.options = this.extend(defaultOptions, options);
+
+        // init
+        this.init();
+    }
+
+    /**
+     * extend easyUploader
+     */
+    easyUploader.prototype = {
+        /**
+         * init function
+         */
+        init: function() {
+            console.log('init function');
+        },
+        /**
+         * extend object function
+         */
+        extend: function(obj, newObj) {
+            for (var key in newObj) {
+                obj[key] = newObj[key];
+            }
+            return obj;
+        }
+    }
+
+    window.easyUploader = easyUploader;
+}());
