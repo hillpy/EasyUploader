b684816fdf3f13097f25416db96480264ad25858
shinn lancelot <945226793@qq.com>
2018-10-09 11:17:24 +0800

    1. example中代码编写完善中

--- example/easyuploader.js
+++ example/easyuploader.js
@@ -1,5 +1,5 @@
 (function() {
-    'use strict';
+    "use strict";
 
     var easyUploader = function(options) {
         if (!(this instanceof easyUploader)) {
@@ -9,10 +9,17 @@
         // default options object
         var defaultOptions = {
             "el": "#file",
+            "file": "#file",
+            "name": "file",
+            "id": "file",
             "method": "post",
             "maxFileSize": "2M",
             "url": "",
+            "autoUpload": true,
         }
+
+        var fileObj = "",
+            elObj = ""; 
 
         // extend options
         this.options = this.extend(defaultOptions, options);
@@ -30,6 +37,18 @@
          */
         init: function() {
             console.log('init function');
+            // render input
+            this.createInput();
+            
+            // bind event 
+            this.bindElToInput();
+
+            // add listen input
+            this.addListenInput();
+
+            // if (this.options.autoUpload) {
+                
+            // }
         },
         /**
          * extend object function
@@ -39,6 +58,37 @@
                 obj[key] = newObj[key];
             }
             return obj;
+        },
+        /**
+         * create input dom
+         */
+        createInput: function() {
+            var input = document.createElement("input");
+            input.type = "file";
+            input.name = this.options.name;
+            input.id = this.options.id;
+            input.setAttribute("style", "display: none;");
+            document.querySelector("body").appendChild(input);
+            this.fileObj = document.querySelector("#" + this.options.id);
+        },
+        /**
+         * bind event
+         */
+        bindElToInput: function() {
+            var _this = this;
+            _this.elObj = document.querySelector(_this.options.el);
+            _this.elObj.addEventListener("click", function() {
+                _this.fileObj.click();
+            })
+        },
+        /**
+         * listen input file
+         */
+        addListenInput: function() {
+            var _this = this;
+            _this.fileObj.addEventListener("change", function() {
+                console.log(_this.fileObj.files[0]);
+            });
         }
     }
 
