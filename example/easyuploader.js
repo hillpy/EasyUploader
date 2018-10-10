07802f9614bfb016e36b8e8cc7bd0f4ac98fba85
shinn lancelot <945226793@qq.com>
2018-10-10 14:34:56 +0800

    1. example中实现简单图片上传压缩功能（待完善），其他文件类型未处理

--- example/easyuploader.js
+++ example/easyuploader.js
@@ -1,7 +1,12 @@
 (function() {
     "use strict";
 
-    var _this = '';
+    var _this = '',
+        fileType = '',
+        fileName = '',
+        canvas = document.createElement('canvas'),
+        context = canvas.getContext('2d'),
+        formData = new FormData();
 
     var easyUploader = function(options) {
         _this = this;
@@ -17,16 +22,15 @@
             "name": "file",
             "id": "file",
             "method": "post",
-            "maxFileSize": "2M",
             "url": "",
             "autoUpload": true,
             "compress": true,
             "resize": {
-                "width": 800,
-                "height": 800
+                "maxWidth": 800,
+                "maxHeight": 800
             },
-            "compress": true,
-            "compressRatio": 0.9
+            "compressRatio": 0.9,
+            "maxFileSize": "2M"
         }
 
         var fileObj = "",
@@ -47,19 +51,12 @@
          * init function
          */
         init: function() {
-            console.log('init function');
             // render input
             _this.createInput();
-            
             // bind event 
             _this.bindElToInput();
-
             // add listen input
             _this.addListenInput();
-
-            // if (_this.options.autoUpload) {
-                
-            // }
         },
         /**
          * extend object function
@@ -78,7 +75,7 @@
             input.type = "file";
             input.name = _this.options.name;
             input.id = _this.options.id;
-            input.setAttribute("style", "display: none;");
+            input.setAttribute("style", "display: none; !important");
             document.querySelector("body").appendChild(input);
             _this.fileObj = document.querySelector("#" + _this.options.id);
         },
@@ -96,8 +93,84 @@
          */
         addListenInput: function() {
             _this.fileObj.addEventListener("change", function() {
-                console.log(_this.fileObj.files[0]);
+                _this.fileObj.files[0];
+                fileType = _this.fileObj.files[0].type;
+                fileName = _this.fileObj.files[0].name;
+                
+                if (fileType.indexOf("image/") >= 0) {
+                    _this.drawAndRenderCanvas();
+                }
             });
+        },
+        /**
+         * drawImage
+         * render Canvas
+         */
+        drawAndRenderCanvas: function() {
+            var reader = new FileReader,
+                image = new Image();
+            
+            reader.readAsDataURL(_this.fileObj.files[0]);
+            reader.onload = function(e) {
+                image.src = this.result;
+            }
+
+            image.onload = function() {
+                if (_this.options.compress) {
+                    if (image.width > _this.options.resize.maxWidth || image.height > _this.options.resize.maxHeight) {
+                        if (image.width > image.height) {
+                            canvas.width = _this.options.resize.maxWidth;
+                            canvas.height = (image.height / image.width) * _this.options.resize.maxWidth;
+                        } else {
+                            canvas.width = (image.width / image.height) * _this.options.resize.maxHeight;
+                            canvas.height = _this.options.resize.maxHeight;
+                        }
+                    }
+                } else {
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+                    _this.options.compressRatio = 1;
+                }
+
+                context.drawImage(image, 0, 0, canvas.width, canvas.height);
+                canvas.setAttribute("style", "display: none !important;");
+                document.querySelector("body").appendChild(canvas);
+                _this.options.autoUpload && _this.uploadCanvas();
+            }
+        },
+        /**
+         * uploader canvas image
+         */
+        uploadCanvas: function() {
+            if (!_this.fileObj.files[0]) {
+                alert("请先选择文件");
+                return;
+            }
+
+            canvas.toBlob(function(blob) {
+                formData.append(_this.options.name, blob, fileName);
+                var xhr = new XMLHttpRequest();
+                xhr.open(_this.options.method, _this.options.url, true);
+                xhr.upload.addEventListener("progress", function(e) {
+                    console.log("上传进度为：" + ((e.loaded / e.total) * 100).toFixed(2) + "%");
+                });
+                xhr.upload.addEventListener("loadstart", function() {
+                    console.log("上传开始"); //只出现一次
+                });
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === 4) {
+                        if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+                            var data = JSON.parse(xhr.responseText);
+                            alert(data.msg);
+                        } else {
+                            console.log("请求失败: " + xhr.status);
+                        }
+                        _this.fileObj.value = '';
+                    }
+                };
+                //xhr.setRequestHeader("Content-type", "multipart/form-data");
+                xhr.send(formData);
+            }, fileType, _this.options.compressRatio);
         }
     }
 
--- example/upload.php
+++ example/upload.php
@@ -1,2 +1,40 @@
 <?php
 
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'post') {
+    $res = array(
+        'code' => 0,
+        'msg' => '上传失败'
+    );
+
+    // 原文件名
+    $fileName = $_FILES['file']['name'];
+    // 临时文件
+    $tmpName = $_FILES['file']['tmp_name'];
+    // 文件类型
+    $type = $_FILES['file']['type'];
+    // 文件大小
+    $size = $_FILES['file']['size'];
+
+    $uploadDir = './file/';
+    if (!file_exists($uploadDir) || !is_dir($uploadDir)) {
+        mkdir($uploadDir);
+    }
+
+    // 打开并读取临时文件
+    $fp = fopen($tmpName, 'r');
+    $file = fread($fp, $size);
+    $data = $file;
+
+    // 打开文件准备写入
+    $newFile = fopen($uploadDir . $fileName, 'w');
+    // 写入二进制流到文件
+    fwrite($newFile, $data);
+    // 关闭文件
+    fclose($newFile);
+
+    $res['code'] = 1;
+    $res['msg'] = '上传成功';
+    echo json_encode($res);
+} else {
+    exit('禁止访问');
+}
