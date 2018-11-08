21afbb1b9177eb702182ab87dce31960b7638b72
shinn lancelot <945226793@qq.com>
2018-11-08 11:01:33 +0800

    1. rollup新增dev和prod配置文件，同时修改npm脚本指令 2. 开源协议修改Apache2.0

---
+++ rollup.config.dev.js
@@ -0,0 +1,8 @@
+export default {
+    input: 'src/main.js',
+    output: {
+        file: 'example/js/easyuploader.js',
+        format: 'iife',   //system, amd, cjs, es, iife, umd
+        name: 'easyUploader'
+    }
+}
