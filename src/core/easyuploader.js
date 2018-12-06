8c3b6231bc74116b753681ceaaf1a0b90ceb244f
shinn lancelot <945226793@qq.com>
2018-12-06 14:49:01 +0800

    1. 新增canvas.toBlob兼容代码

--- src/core/easyuploader.js
+++ src/core/easyuploader.js
@@ -1,3 +1,17 @@
+if (!HTMLCanvasElement.prototype.toBlob) {
+    Object.defineProperty(HTMLCanvasElement.prototype, "toBlob", {
+        value: function(callback, type, quality) {
+            var binStr = atob(this.toDataURL(type, quality).split(",")[1]),
+                len = binStr.length,
+                arr = new Uint8Array(len);
+            for (var i = 0; i < len; i++) {
+                arr[i] = binStr.charCodeAt(i);
+            }
+            callback(new Blob([arr], { type: type || "image/png" }));
+        }
+    });
+}
+
 var easyUploader = function(options) {
     if (!(this instanceof easyUploader)) {
         return new easyUploader(options);
