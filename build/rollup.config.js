7a97b71cb270ec59cdb1feedba75fdee9cbba874
shinn lancelot <945226793@qq.com>
2019-02-11 12:31:05 +0800

    build: update npm package such as rollup

---
+++ build/rollup.config.js
@@ -0,0 +1,63 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import buble from 'rollup-plugin-buble'
+import { terser } from 'rollup-plugin-terser'
+import flow from 'rollup-plugin-flow-no-whitespace'
+import json from 'rollup-plugin-json'
+import pkg from '../package.json'
+
+const production = !process.env.ROLLUP_WATCH
+const version = process.env.VERSION || pkg.version
+const libName = 'EasyUploader' || pkg.name
+const birthYear = 2018
+const banner =
+    '/*\n' +
+    ` * ${libName} v${version}\n` +
+    ` * (c) ${birthYear}-${new Date().getFullYear()} ${pkg.author}\n` +
+    ` * Released under the ${pkg.license} License.\n` +
+    ' */'
+const outPath = './dist/'
+const outFileInfo = {
+  'dev': {
+    'cjs': pkg.main,
+    'esm': pkg.module,
+    'umd': pkg.browser
+  },
+  'prod': {
+    'cjs': outPath + libName.toLowerCase() + '.cjs.min.js',
+    'esm': outPath + libName.toLowerCase() + '.esm.min.js',
+    'umd': outPath + libName.toLowerCase() + '.min.js'
+  }
+}
+
+export default {
+  input: 'src/main.js',
+  output: [
+    {
+      file: production ? outFileInfo.prod.cjs : outFileInfo.dev.cjs,
+      format: 'cjs',
+      banner: banner,
+      name: libName
+    },
+    {
+      file: production ? outFileInfo.prod.esm : outFileInfo.dev.esm,
+      format: 'esm',
+      banner: banner,
+      name: libName
+    },
+    {
+      file: production ? outFileInfo.prod.umd : outFileInfo.dev.umd,
+      format: 'umd',
+      banner: banner,
+      name: libName
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    buble(),
+    production && terser(),
+    flow(),
+    json()
+  ]
+}
--- dist/easyuploader.js
+++ dist/easyuploader.js
@@ -6,8 +6,8 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global.EasyUploader = factory());
-}(this, (function () { 'use strict';
+  (global = global || self, global.EasyUploader = factory());
+}(this, function () { 'use strict';
 
   /**
    * The default options file.
@@ -689,4 +689,4 @@
 
   return EasyUploader;
 
-})));
+}));
